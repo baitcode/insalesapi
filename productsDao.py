138510a4bc9f1c69931a9557ba6faf0ddf006aa7
bait <batiyiv@gmail.com>
2011-05-22 02:33:18 +0400

    * changed Dao's methods signature

--- productsDao.py
+++ productsDao.py
@@ -6,10 +6,10 @@ class ProductsDao(BaseDao):
     def __init__(self, api_key, api_password, host):
         super(ProductsDao, self).__init__('product', api_key, api_password, host)
 
-    def getProductList(self, lang=None):
+    def getList(self, lang=None):
         products = self.read('/admin/products.xml', lang)
         return Product.wrapCollection(products)
 
-    def addProduct(self, product, lang=None):
+    def add(self, product, lang=None):
         products = self.create('/admin/products.xml', product, lang)
         return Product.wrapCollection(products)
